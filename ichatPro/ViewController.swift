c5ea1925445e67fc54189be2f828b61edb193836
Abdur Rehman Atif <aleemtarder@gmail.com>
2016-06-24 18:56:59 +0500

    FacebookButtonImplimentWithitsOwnButton

--- ichatPro/ViewController.swift
+++ ichatPro/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let loginButton: FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center
+        self.view!.addSubview(loginButton)
 
+    }
+    
+   
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
